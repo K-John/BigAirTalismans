bf304830698268a73f0d9f7641713dc48ca8a641
Kendall Johnson <kenjohnson1994@gmail.com>
2023-02-19 14:40:45 -0600

    Dont pickup if moving

--- api/tasks/PickupTask.java
+++ api/tasks/PickupTask.java
@@ -1,6 +1,9 @@
 package scripts.api.tasks;
 
+import org.tribot.script.sdk.MyPlayer;
+import org.tribot.script.sdk.Waiting;
 import org.tribot.script.sdk.query.Query;
+import org.tribot.script.sdk.util.TribotRandom;
 import scripts.Task;
 import scripts.Priority;
 import scripts.api.data.Constants;
@@ -22,6 +25,12 @@ public class PickupTask implements Task {
     public void execute() {
         Vars.get().setStatus("Picking up Air Talismans");
 
+        // Wait until we're done moving to click another
+        if (MyPlayer.isMoving()) {
+            Waiting.waitUntil(TribotRandom.uniform(200, 800), () -> !MyPlayer.isMoving());
+        }
+
+        // Pick up Talisman
         Query.groundItems()
                 .inArea(Constants.DUKES_ROOM)
                 .idEquals(Constants.NOTED_AIR_TALISMAN, Constants.AIR_TALISMAN)
