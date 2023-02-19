97efe3d567c4b46256d9db18b1e3d04e89e76e70
Kendall Johnson <kenjohnson1994@gmail.com>
2023-02-19 15:26:21 -0600

    Modify area coordinates

--- api/data/Constants.java
+++ api/data/Constants.java
@@ -8,7 +8,7 @@ public class Constants {
     public static final int AIR_TALISMAN = 1438;
     public static final int NOTED_AIR_TALISMAN = 1439;
     public static final int DUKE = 815;
-    public static final Area DUKES_ROOM = Area.fromRectangle(new WorldTile(3208, 3218, 1), new WorldTile(3212, 3225, 1));
+    public static final Area DUKES_ROOM = Area.fromRectangle(new WorldTile(3208, 3219, 1), new WorldTile(3212, 3225, 1));
     public static final Area BANK = Area.fromRectangle(new WorldTile(3208, 3214, 2), new WorldTile(3210, 3220, 2));
 
     public static final String[] DUKE_DIALOGUE = {"What did you want me to do again?"};
