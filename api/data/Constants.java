b9a2465a578878f108877cacc1b07a7f71644c74
Kendall Johnson <kenjohnson1994@gmail.com>
2023-02-19 21:58:05 -0600

    Update Dialogue Constant

--- api/data/Constants.java
+++ api/data/Constants.java
@@ -11,5 +11,5 @@ public class Constants {
     public static final Area DUKES_ROOM = Area.fromRectangle(new WorldTile(3208, 3219, 1), new WorldTile(3212, 3225, 1));
     public static final Area BANK = Area.fromRectangle(new WorldTile(3208, 3214, 2), new WorldTile(3210, 3220, 2));
 
-    public static final String[] DUKE_DIALOGUE = {"What did you want me to do again?"};
+    public static final String DUKE_DIALOGUE = "What did you want me to do again?";
 }
